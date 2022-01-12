d550e22dbcad60d1af5481cb063c5aeece57f762
FrankLuo426 <caesarfrank@hotmail.com>
2022-01-12 23:09:14 +0800

    E01

---
+++ Exercise/E01/E01/Main.cpp
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------
+-----------
+Comments about this program
+-----------------------------------------------------------------------------------------
+---------*/
+#include "Main.h"
+int main()
+{
+	std::cout << "Hello World\n";
+	//Ending the program
+	std::cout << "Press enter to finish";
+	getchar();
+	return 0;
+}
