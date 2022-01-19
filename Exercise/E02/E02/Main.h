b6f7550473185ef9f7c575f93baeb2de1f3e6d2a
FrankLuo426 <caesarfrank@hotmail.com>
2022-01-20 00:07:22 +0800

    Exercise02, 未完成

---
+++ Exercise/E02/E02/Main.h
@@ -0,0 +1,18 @@
+#pragma once
+/*---------------------------------------------------------------------------------------
+-----------
+This project was generated in 2018
+-----------------------------------------------------------------------------------------
+---------*/
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <iostream>
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+#include "GLM\glm.hpp"
+#include "OpenGL-Tutorials\shader.hpp"
+#endif //_MAIN_H
